7e1fc7dae873851e209e5bb1b1102c77e38c1e6e
whiskaz112 <111265961+whiskaz112@users.noreply.github.com>
2023-04-28 18:37:35 +0700

    Update Cart.py

--- Cart.py
+++ Cart.py
@@ -8,9 +8,13 @@ class Cart:
     def add_product(self, product, quantity = 1):
         if product in self.items.keys():
             self.items[product] += quantity
+            for pro in procat.product_catalog:
+                if pro.name == product:
+                    pro.quantity -= quantity
         else:
             self.items.update({product : quantity})
-        print(self.items)
+        
+        
     
     def remove_product(self, product, quantity):
         if product in self.items:
@@ -35,41 +39,34 @@ class Cart:
         total_price = 0
         for product ,quantity in self.items.items():
             total_price += product.price * quantity
-        print (total_price)
+        #print (total_price)
         return total_price
             # total_price += product.price * quantity
         # return total_price
 
-    def apply_discount_code(self):
-        return self.calculate_price() * 
         
 
             
         
 product1 = Product("key001",2000,2000,"blahblah1",20,"keyboard")
-product2 = Product("key002",300,2000,"blahblah",20,"keyboard")
+product2 = Product("key002",3000,1500,"blahblah",20,"keyboard")
 procat = ProductCatalog()
 procat.add_product_to_catalog(product1)
 procat.add_product_to_catalog(product2)
-procat.view_catalog()
-procat.get_price("key001")
-
+#procat.view_catalog()
 
 cart = Cart()
 
 cart.add_product(product1, 2)
 cart.add_product(product2, 3)
 cart.add_product(product1, 2)
-
-cart.get_discount()
-
-
+# procat.view_catalog()
 cart.remove_product(product2, 4)
 
-cart.view_cart()
+#cart.view_cart()
 
 # cart.clear_cart()
-cart.calculate_price()
+#
 
 
 
