5bb82d7e54b2a743b0086c95c3a4a75042088710
whiskaz112 <111265961+whiskaz112@users.noreply.github.com>
2023-04-28 15:44:34 +0700

    Update DiscountCode.py

--- DiscountCode.py
+++ DiscountCode.py
@@ -2,3 +2,24 @@ class DiscountCode:
     def __init__(self, code, discount_percentage):
         self.__code = code
         self.__discount_percentage = discount_percentage
+        
+
+    @property
+    def code(self):
+        return self.__code
+    @code.setter
+    def set_code(self,code):
+        self.code = code
+
+
+    @property
+    def discount_percentage(self):
+        return self.__discount_percentage
+    @discount_percentage.setter
+    def set_discount_percentage(self,discount_percentage):
+        self.discount_percentage = discount_percentage
+
+
+
+
+        
