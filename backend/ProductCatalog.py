0cca8076c3d4d7bea54ba392c609e08e85ff831a
pkstpm <pongsapat028@gmail.com>
2023-04-29 22:47:22 +0700

    import file to main

--- backend/ProductCatalog.py
+++ backend/ProductCatalog.py
@@ -1,3 +1,5 @@
+from Product import *
+
 class ProductCatalog:
     def __init__(self):
         self.__products = []
@@ -9,3 +11,7 @@ class ProductCatalog:
     def products(self, new_products):
         self.__products = new_products
         return self.__products
+    
+    def add_product(self, product):
+        if isinstance(product,Product):
+            self.products.append(product)
---
+++ backend/main.py
@@ -0,0 +1,6 @@
+from Account import *
+from Accountlist import *
+from Cart import *
+from Product import *
+from ProductCatalog import *
+from Review import *
